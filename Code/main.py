e71e308fad75e7dc07253f2d16ad1efc5d73604f
Tanmay1005 <ambegaokart@gmail.com>
2024-05-03 09:47:25 -0400

    Refactored path changes in main.py

--- Code/main.py
+++ Code/main.py
@@ -8,14 +8,20 @@ from scripts.keypoint_distance import keypoint_distances
 from scripts.match import visualize_matches
 from scripts.ransac import import_into_colmap
 from scripts.utils import plot_reconstruction,colmap_dense_reconstruction,save_rot_tra_info
-
-PATH = '/home/ubuntu/DepthArt/saj'
-EXT = 'jpg'
-PATH_FEATURES = '/home/ubuntu/DepthArt/features'
-DINO_PATH = '/home/ubuntu/DepthArt/dinov2/pytorch/base/1'
+import os
+base_dir=os.getcwd()
+scene='bike'
+dataset='haiper'
+PATH = os.path.join(base_dir,'train',f'{dataset}',f'{scene}','images')
+# PATH=f'/home/ubuntu/Final-Project-Group2/Code/train/{dataset}/{scene}/images'
+print(PATH)
+EXT = 'jpeg'
+PATH_FEATURES = os.path.join(base_dir,'features')
+DINO_PATH = os.path.join(base_dir,'dinov2','pytorch','base','1')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Get Image Pairs for Matching using DINO
+print(Path(PATH).glob(f'*.{EXT}'))
 images_list = list(Path(PATH).glob(f'*.{EXT}'))
 index_pairs = get_image_pairs(images_list, DINO_PATH)
 
@@ -27,13 +33,16 @@ detect_keypoints(images_list, feature_dir, device=device)
 keypoint_distances(images_list, index_pairs, feature_dir, verbose=False, device=device)
 
 # Visualise Image matching
-idx1, idx2 = index_pairs[2]
+idx1, idx2 = index_pairs[0]
 visualize_matches(images_list, idx1, idx2, feature_dir)
 
+base_dir = Path.cwd()
 
+database_path = base_dir / f'colmap_{scene}.db'
+# Ensure database path does not exist before continuing
+if database_path.exists():
+    database_path.unlink()
 
-# Import into Colmap
-database_path = "colmap_saj.db"
 images_dir = images_list[0].parent
 import_into_colmap(
     images_dir,
@@ -60,9 +69,9 @@ maps = pycolmap.incremental_mapping(
 save_rot_tra_info(maps, 'rot_tra_info.csv')
 
 # Visualize the 3D reconstruction
-# plot_reconstruction(maps[0], 'Reconstructionsaj.html')
+plot_reconstruction(maps[0], f'Reconstruction_{scene}.html')
 
-
-# Dense Reconstruction
-# colmap_dense_reconstruction(images_dir, database_path, Path.cwd())
+#
+# # Dense Reconstruction
+# # colmap_dense_reconstruction(images_dir, database_path, Path.cwd())
 
